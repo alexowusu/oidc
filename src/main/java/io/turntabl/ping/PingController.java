4c6b49d036362336a36b6d52d9af55c9be0ee39b
pkerbynn <john.erbynn@gmail.com>
2020-01-29 06:46:26 +0000

    [feat]: add auth header to mapping route

--- src/main/java/io/turntabl/ping/PingController.java
+++ src/main/java/io/turntabl/ping/PingController.java
@@ -2,9 +2,12 @@ package io.turntabl.ping;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import io.turntabl.tokenVerifier.GoogleIdVerifier;
+//import io.turntabl.tokenVerifier.GoogleIdVerifier;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
 
 @Api
 @RestController
@@ -15,5 +18,31 @@ public class PingController {
     @GetMapping(value = "v1/api/ping")
     public String ping(){
         return "Hello pong from Ping endpoint";
+    }
+
+    @CrossOrigin
+    @ApiOperation("verify token")
+    @PostMapping(value = "v1/api/tokenVerify")
+    public String verify(@RequestHeader("authorization") String tokenId) {
+        tokenId = tokenId.replace("Bearer ", "");
+        System.out.println("***** " + tokenId);
+        String payload = tokenId.split("\\.")[1];
+
+        if(!(tokenId.isEmpty())) {
+            System.out.println("....not empty....");
+
+            try {
+                String decode = new String(Base64.decodeBase64(payload), "UTF-8");
+                System.out.println("###########");
+                System.out.println(decode);
+                System.out.println("###########");
+
+                return decode;
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return "Invalid token";
+
     }
 }
